f80fb2125c77cf5849bf028cb6783064f970f503
Carlos <jmoreno@elfinanciero.com.mx>
2018-03-22 13:27:58 -0600

    Add nodemon and script

--- index.js
+++ index.js
@@ -8,11 +8,41 @@ const express = require("express"),
     _ = require("lodash"),
     app = express();
 
+const APIS = require("./util");
+
+// Helper for parse HTML
+app.locals.htmlParsed = html => _.escape(html).replace(/\n/g, "<br>");
+// Use static assets
+app.use(express.static("public"));
+// Setting up Body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// Setting up UPDATE method-override
+app.use(methodOverride("_method"));
+// Setting up cloudinanry
+cloudinary.config(APIS.CLOUDINARY);
 
-const APIS = require('./utils');
+// Passport configuration
+app.use(
+    require("express-session")({
+        secret: "Site of the year 2018",
+        resave: false,
+        saveUninitialized: false
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
+// Spread id on the routes
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
+
+// ********* ROUTES ************
 app.get('/', (req, res) => {
     res.send('API UP');
 });
