eb7add80b0d4f55c8070a3e4be1e58db3219eaf3
Carlos <jmoreno@elfinanciero.com.mx>
2018-03-23 11:10:38 -0600

    Add image

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 var express = require('express');
-var router = express.Router();
 var db = require('./../models');
+
+const router = express.Router();
 
 router.get('/', (req,res) => {
     db.Post.paginate({}, { page: 1, limit: 10 })
@@ -10,6 +11,33 @@ router.get('/', (req,res) => {
     .catch(err => {
         res.send(err);
     });
-})
+});
+
+router.post('/', (req,res) => {
+    const { title, content, image } = req.body;
+    // const { filename } = req.file;
+    const resume = content.substring(0, 250);
+    
+    console.log(title, content, image)
+    // ***** Upload image *******
+    // cloudinary.uploader.upload(`public/uploads/${filename}`, (result) => {
+    //     const imagePath = result.url;
+    // });
+
+    const userPost = {
+        title: title,
+        image: image,
+        content: content,
+        resume: resume
+    };
+    
+    db.Post.create(userPost)
+    .then(post => {
+        res.json(post);
+    })
+    .catch(err => {
+        res.send(err);
+    });
+});
 
 module.exports = router;
