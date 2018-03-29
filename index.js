163763db3571f61ad32d1a8bc43c2b26082b5a52
Carlos <jmoreno@elfinanciero.com.mx>
2018-03-28 18:42:45 -0600

    l

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ app.use(methodOverride("_method"));
 // Setting up cloudinanry
 cloudinary.config(APIS.CLOUDINARY);
 
+//CORS
+app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
 // Passport configuration
 app.use(
     require("express-session")({
