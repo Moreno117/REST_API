518cf01c86bae67f473cf1dc9ec3d8e8e79f5009
Carlos <jmoreno@elfinanciero.com.mx>
2018-03-31 21:39:24 -0600

    Add header for allow methods PUT and DELETE

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ cloudinary.config(APIS.CLOUDINARY);
 //CORS
 app.use((req,res,next) => {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
