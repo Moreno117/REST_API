8e3e66e3ef443e1a7a9e0ccec4ca59020a85562b
Carlos <jmoreno@elfinanciero.com.mx>
2018-04-04 16:33:43 -0500

    Add unauthorized status

--- helpers/middleware/index.js
+++ helpers/middleware/index.js
@@ -2,5 +2,5 @@ module.exports = function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
-    res.send({ message: 'User no Authenticated', code: 401 });
+    res.status(401).end();
 }
