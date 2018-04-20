987ae61b3ce593feba33c5517239acd28da10084
Carlos <jmoreno@elfinanciero.com.mx>
2018-04-19 19:54:18 -0500

    Add Auth header

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ cloudinary.config(APIS.CLOUDINARY);
 app.use((req,res,next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
     next();
 })
 
